2cf6516214f6575116092bfd9968fdb81ac7bf15
lewchiapang <chiapang@buffalo.edu>
2014-07-14 16:25:29 -0400

    d

---
+++ Lab07/src/vMem.java
@@ -0,0 +1,25 @@
+
+public class vMem {
+
+	public vMem(int ARRAYSIZE, int j) {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public void put(int index, long value) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public long get(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+
+	
+	
+	
+}
