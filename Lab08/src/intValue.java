c93242d45e386ee53257379883debd6802b45ab0
lewchiapang <chiapang@buffalo.edu>
2014-07-22 16:41:20 -0400

    Encryption

--- Lab08/src/intValue.java
+++ Lab08/src/intValue.java
@@ -1,4 +1,18 @@
 
 public class intValue {
-
+	
+	public int multiply(int number1, int number2){
+		int n = number1 * number2;
+		return n;
+	}
+	
+	public int add(int number1, int number2){
+		int n = number1 + number2;
+		return n;
+	}
+	
+	public int pow(long number1, long number2){
+		int n = (int) Math.pow(number1, number2);
+		return n;
+	}
 }
