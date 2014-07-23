5623722798d367c0c40ee1b1803022b43c4b238c
lewchiapang <chiapang@buffalo.edu>
2014-07-23 02:21:29 -0400

    g

---
+++ Lab08/src/Encryption.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Encryption {
+	public static void main(String [] args){
+		
+		intValue iv = new intValue();
+		Scanner inputScanner = new Scanner(System.in);
+		
+		final int NUMBERS = 30;
+		long[] numbers = new long[NUMBERS];
+		
+		
+		System.out.println("Please enter the first digits: ");
+		int num1 = inputScanner.nextInt();
+		
+		System.out.println("Pleasae enter the second digits: ");
+		int num2 = inputScanner.nextInt();
+		
+				
+//		StringBuilder sb = new StringBuilder(0);
+//		sb.append(num1);
+//		sb.append(num2);
+		
+		
+		
+		switch (c){
+		
+		case 'M':
+		System.out.println(iv.multiply(num1, num2));
+//		System.out.println(sb.toString());
+		}
+		
+	}
+
+}
