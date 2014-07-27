c4775ff1498ba20ac7849fe7490e0fece0c8d5a3
aaron <aaron@aaron-virtual-machine>
2014-07-27 17:29:31 -0400

    d

--- Lab08/src/Encryption.java
+++ Lab08/src/Encryption.java
@@ -1,20 +1,69 @@
-import java.util.Scanner;
+import java.util.*;
+//import java.math.*;
+import java.math.BigInteger;
 
 public class Encryption {
 	public static void main(String [] args){
 		
-		intValue iv = new intValue();
-		Scanner inputScanner = new Scanner(System.in);
+//		intValue iv = new intValue();
+//		Scanner inputScanner = new Scanner(System.in);
+//		
+//		final int NUMBERS = 30;
+//		String[] numbers = new String[NUMBERS];
+//		
+//		System.out.println("Please enter the first digits: ");
+//		int num1 = inputScanner.nextInt();
+//		
+//		System.out.println("Please enter the second digits: ");
+//		int num2 = inputScanner.nextInt();
 		
-		final int NUMBERS = 30;
-		long[] numbers = new long[NUMBERS];
+		Scanner in = new Scanner(System.in);
+		intValue v1 = new intValue();
+		intValue v2 = new intValue();
 		
 		
-		System.out.println("Please enter the first digits: ");
-		int num1 = inputScanner.nextInt();
+		System.out.print("Please enter the first value: ");
+		String str1 = in.nextLine();
 		
-		System.out.println("Pleasae enter the second digits: ");
-		int num2 = inputScanner.nextInt();
+		v1.num1 = new BigInteger(str1);
+		System.out.print("Please enter the second value: ");
+		String str2 = in.nextLine();
+		
+		v2.num1 = new BigInteger(str2);
+		in.close();
+//		System.out.println(v.num1);
+//		System.out.println(v.num2);
+		
+		try{
+			
+//		int newnum1 = Integer.parseInt(v.num1);
+//		int newnum2 = Integer.parseInt(v.num2);
+//		
+//		while(newnum1 < 100){
+//			System.out.println("In the loop");
+//			newnum1 = newnum1 + 100;
+//		}
+//		while(newnum2 < 100){
+//			System.out.println("Inside the loop");
+//			newnum2 = newnum2 + 100;
+//		}
+//				
+//			newnum1 = in.nextInt();
+//			newnum2 = in.nextInt();
+//		}
+		
+			intValue v3 = new intValue();
+			v3 = v1.multiply(v2);
+			System.out.print("The product is: " + v3.num1);
+
+		}
+		
+		catch(NumberFormatException ex){
+			System.out.print("Invalid input, please re-enter a new number.");
+			return;
+		}
+
+
 		
 				
 //		StringBuilder sb = new StringBuilder(0);
@@ -22,14 +71,9 @@ public class Encryption {
 //		sb.append(num2);
 		
 		
-		
-		switch (c){
-		
-		case 'M':
-		System.out.println(iv.multiply(num1, num2));
+//		System.out.println(iv.multiply(num1, num2));
 //		System.out.println(sb.toString());
 		}
 		
 	}
 
-}
--- Lab08/src/intValue.java
+++ Lab08/src/intValue.java
@@ -1,18 +1,39 @@
+import java.math.BigInteger;
 
 public class intValue {
 	
-	public int multiply(int number1, int number2){
-		int n = number1 * number2;
-		return n;
-	}
+	BigInteger num1;
+	//String num2;
+	String result;
 	
-	public int add(int number1, int number2){
-		int n = number1 + number2;
-		return n;
-	}
 	
-	public int pow(long number1, long number2){
-		int n = (int) Math.pow(number1, number2);
-		return n;
+	
+	 
+	public intValue multiply(intValue n){
+		
+		
+		BigInteger result ;
+		BigInteger var1, var2;
+		var1 = this.num1;
+		var2 = n.num1;
+		result = var1.multiply(var2);
+		intValue res = new intValue();
+		res.num1 = result;
+		return res;
 	}
+//	public intValue addition(intValue n){
+//		int newnum = Integer.parseInt(this.num1) + Integer.parseInt(n.num2);
+//		result = Integer.toString(newnum);
+//		return n;
+//	}
+//	
+//	public int add(int number1, int number2){
+//		int n = number1 + number2;
+//		return n;
+//	}
+	
+//	public int pow(long number1, long number2){
+//		int n = (int) Math.pow(number1, number2);
+//		return n;
+//	}
 }
